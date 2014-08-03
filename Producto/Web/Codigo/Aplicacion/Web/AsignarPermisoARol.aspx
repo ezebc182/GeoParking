030072859d9fb584a1b7f8057748cbf07391ea34
nachiof <nachiof@gmail.com>
2014-08-02 21:58:01 -0300

    asignar permisos a rol historia 27
    componentes UI (no todos) y gran parte del back end

---
+++ Producto/Web/Codigo/Aplicacion/Web/AsignarPermisoARol.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AsignarPermisoARol.aspx.cs" Inherits="Web.AsignarPermisoARol" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="TopContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+    <h3 style="position: relative; top: 140px; right: -50%;">Asignar permiso a rol</h3>
+     <div class="form-group" style="position:relative; top:160px; right:-40%; height:500px; width:500px" >
+        <div class="col-sm-10 col-md-10 col-lg-10">
+            <label for="ddlRol" class="col-sm-2 col-md-2 col-lg-2 control-label">Rol</label>
+            <asp:DropDownList ID="ddlRol" CssClass="form-control required" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlRol_SelectedIndexChanged">
+            </asp:DropDownList>
+
+        </div>
+        <div class="col-sm-10 col-md-10 col-lg-10">
+            <label for="ddlRol" class="col-sm-2 col-md-2 col-lg-2 control-label">Permisos</label>
+            <div style="position: relative;top: 25px;">
+                <asp:CheckBoxList ID="cblPermiso" runat="server">
+            </asp:CheckBoxList>
+            </div>
+        </div>
+
+     </div>
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="helpContent" runat="server">
+</asp:Content>
---
+++ Producto/Web/Codigo/Aplicacion/Web/AsignarPermisoARol.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Entidades;
+using ReglasDeNegocio;
+using Web.Util;
+
+namespace Web
+{
+    public partial class AsignarPermisoARol : System.Web.UI.Page
+    {
+        GestorRol gestor = new GestorRol();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                cargarComboRol();
+                cargarListadoCheckBoxPermisos();
+            }
+        }
+        public void cargarComboRol()
+        {
+            FormHelper.CargarCombo(ddlRol, gestor.BuscarRoles(), "Nombre", "Id", "Seleccione");
+        }
+        public void cargarListadoCheckBoxPermisos()
+        {
+            //Rol rol = gestor.BuscarRol(int.Parse(ddlRol.SelectedValue));
+            cblPermiso.DataSource = gestor.BuscarPermisos();
+            cblPermiso.DataTextField = "Nombre";
+            cblPermiso.DataBind();
+            if (ddlRol.SelectedIndex != 0)
+            {
+                seleccionarPermisosDeRol();
+            }
+        }
+        public void seleccionarPermisosDeRol()
+        {
+            Rol rol = gestor.BuscarRol(int.Parse(ddlRol.SelectedValue));
+            IList<Permiso> permisos = gestor.BuscarPermisosPorRol(rol);
+            foreach (Permiso permiso in permisos)
+            {
+                cblPermiso.Items.FindByValue(permiso.Id.ToString()).Selected = true;
+            }
+        }
+
+        protected void ddlRol_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            seleccionarPermisosDeRol();
+        }
+
+    }
+}
