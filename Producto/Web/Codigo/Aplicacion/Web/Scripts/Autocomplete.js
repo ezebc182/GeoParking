3bb2c3972fd9a4104dce4f94153a7b6cbf3733d2
lucastoneatto <lucastoneatto@gmail.com>
2014-08-06 00:05:33 -0300

    US2 Archivo de js para el autocomplete
    No funciona porque la pagina no lo procesa, pero al idea es hacerlo
    funcionar , y sacar el codigo de la pagina

---
+++ Producto/Web/Codigo/Aplicacion/Web/Scripts/Autocomplete.js
@@ -0,0 +1,19 @@
+﻿function SearchText() {
+    $(".autosuggest").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                type: "POST",
+                contentType: "application/json; charset=utf-8",
+                url: "Index.aspx/GetNombreCiudades",
+                data: "{'username':'" + document.getElementById('txtBuscar').value + "'}",
+                dataType: "json",
+                success: function (data) {
+                    response(data.d);
+                },
+                error: function (result) {
+                    alert("Error");
+                }
+            });
+        }
+    });
+}
