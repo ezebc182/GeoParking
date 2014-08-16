8a2b0084edd583a6921b2d0d6b9b5b2c372f7130
lucastoneatto <lucastoneatto@gmail.com>
2014-08-16 01:54:03 -0300

    Creacion de clases para mapeo JSON en Vista Web
    se crearon 5 clases que suplanatan a las entidaes playa de
    estacionamiento, servicio, direccion, precio y horario. se necesita para
    poder pasar la informacion a la vista aspx.. en posteriores sprint si se
    encuentra una mejor solucion se modificara

---
+++ Producto/Web/Codigo/Aplicacion/Web/ClasesJsonVista/HorarioJSON.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.ClasesJsonVista
+{
+    public class HorarioJSON
+    {
+        public string Dia { get; set; }
+        public string HoraDesde { get; set; }
+        public string HoraHasta { get; set; }
+    }
+}
