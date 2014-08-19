cf011cb19541aae3114470a97b011c3c03128d03
lucastoneatto <lucastoneatto@gmail.com>
2014-08-19 15:31:10 -0300

    Creacion y referenciacion de web service

---
+++ Producto/Web/Codigo/Aplicacion/Negocio/WebServices/GeoService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="GeoService.asmx.cs" Class="WebServices.Service1" %>
--- Producto/Web/Codigo/Aplicacion/Web/BusquedaPlaya.aspx.cs
+++ Producto/Web/Codigo/Aplicacion/Web/BusquedaPlaya.aspx.cs
@@ -15,8 +15,15 @@ namespace Web
 {
     public partial class BusquedaPlaya : System.Web.UI.Page
     {
-        private static GestorBusquedaPlayas gestor;
+        //Ciudad buscada por el usuario
         private static String ciudadBuscada;
+
+        //gestor de busqueda de playas
+        private static GestorBusquedaPlayas gestor;
+
+        //Referencia al servicio web "GeoService"
+        private static GeoService.Service1 geoServicio = new GeoService.Service1();
+
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -63,129 +70,25 @@ namespace Web
             FormHelper.CargarCombo(ddlDiasAtencion, gestor.BuscarDiasDeAtencion(), "Nombre", "Id", "Seleccione");
         }
 
-        /// <summary>
-        /// Obtiene las playas de estacionamiento de la cudad seleccionada
-        /// </summary>
-        /// <param name="ciudad">ciudad filtro</param>
-        /// <returns>playas de estacionamiento ubicadas en esa ciudad</returns>
+        /*NOTA IMPORTANTE:
+         Se ha heliminado el codigo de busqueda y filtrado de playas, 
+         el mismo a pasado a formar parte de un servicio web "GeoService".
+         Por motivos de implementacion aun los meotodos de esta pagina se
+         siguen implementando como WebMethod, ya que aun no se puede realizar
+         la conexion directa de ajax al servicio web. Lo importante es que 
+         el codigo ya no esta en behind*/
         [WebMethod]
-        public static List<PlayaJSON> ObtenerPlayasDeCiudad()
+        public static string ObtenerPlayasDeCiudad()
         {
-            //busco en la BD
-            IList<PlayaDeEstacionamiento> playas = new List<PlayaDeEstacionamiento>();
-            playas = (List<PlayaDeEstacionamiento>)gestor.buscarPlayasPorCiudad(ciudadBuscada);
-
-            //mapeo a objeto serializable
-            List<PlayaJSON> playasJSON = new List<PlayaJSON>();
-            playasJSON=mapearEntityAJSON(playas);            
-
-            return playasJSON;
-
+            string playasDeCiudad = geoServicio.ObtenerPlayasDeCiudad(ciudadBuscada);
+            return playasDeCiudad;
         }
 
-        /// <summary>
-        /// Busco la playas en la BD que cumplan con los filtros
-        /// </summary>
-        /// <param name="tipoPlaya">tipo de playa</param>
-        /// <param name="tipoVehiculo"> el tipo de vehiculo</param>
-        /// <param name="diaAtencion">dia de atencion</param>
-        /// <param name="precioDesde">precio base</param>
-        /// <param name="precioHasta">precio maximo</param>
-        /// <param name="horaDesde">hora de apertura</param>
-        /// <param name="horaHasta">hora de cierre</param>
-        /// <returns>lista de playas filtradas</returns>
         [WebMethod]
-        public static List<PlayaJSON> ObtenerPlayasDeCiudadPorFiltro(int tipoPlaya, int tipoVehiculo, int diaAtencion, string precioDesde, string precioHasta, int horaDesde, int horaHasta)
+        public static string ObtenerPlayasDeCiudadPorFiltro(int tipoPlaya, int tipoVehiculo, int diaAtencion, string precioDesde, string precioHasta, int horaDesde, int horaHasta)
         {
-            //busco en la BD
-            IList<PlayaDeEstacionamiento> playas = new List<PlayaDeEstacionamiento>();
-                       
-            playas = (List<PlayaDeEstacionamiento>)gestor.buscarPlayasPorFiltro(ciudadBuscada, tipoPlaya, tipoVehiculo, diaAtencion,Decimal.Parse(precioDesde),Decimal.Parse(precioHasta),horaDesde,horaHasta);
-           
-            //mapeo a objeto serializable
-            List<PlayaJSON> playasJSON = new List<PlayaJSON>();
-            playasJSON = mapearEntityAJSON(playas);
-
-            return playasJSON;
-
-        }
-
-        /// <summary>
-        /// Mapea un objeto entity en un objeto serializable a JSON
-        /// </summary>
-        /// <param name="playasEntity">objeto entity</param>
-        /// <returns>lista de objetos serializables a JSON</returns>
-        public static List<PlayaJSON> mapearEntityAJSON(IList<PlayaDeEstacionamiento>playasEntity)
-        {
-            List<PlayaJSON> playas = new List<PlayaJSON>();            
-
-            foreach (var p in playasEntity)
-            {
-                PlayaJSON playa = new PlayaJSON();
-
-                //datos basicos
-                playa.Nombre = p.Nombre;
-                playa.Mail = p.Mail;
-                playa.Telefono = p.Telefono;
-                playa.TipoPlaya = p.TipoPlayaStr;
-
-                //datos geograficos
-                playa.Latitud = p.Direcciones[0].Latitud;
-                playa.Longitud = p.Direcciones[0].Longitud;
-                
-                
-
-                //DIRECCIONES
-                playa.Direcciones = new List<DireccionJSON>();
-                foreach (var d in p.Direcciones)
-                {
-                    DireccionJSON direccion = new DireccionJSON();
-                    direccion.Calle = d.Calle;
-                    direccion.Numero = d.Numero;
-
-                    if(direccion != null)
-                        playa.Direcciones.Add(direccion);
-                }
-
-                //SERVICIOS
-                playa.Servicios = new List<ServicioJSON>();
-                foreach (var s in p.Servicios)
-                {
-                    ServicioJSON servicio = new ServicioJSON();
-                    servicio.TipoVehiculo = s.TipoVehiculoStr;
-                    servicio.Capacidad = s.Capacidad;
-
-                    playa.Servicios.Add(servicio);
-                }
-
-                //HORARIOS
-                playa.Horarios = new List<HorarioJSON>();
-                foreach (var h in p.Horarios)
-                {
-                    HorarioJSON horario = new HorarioJSON();
-                    horario.Dia = h.DiaAtencionStr;
-                    horario.HoraDesde = h.HoraDesde;
-                    horario.HoraHasta = h.HoraHasta;
-
-                    playa.Horarios.Add(horario);
-                }
-
-                //PRECIOS
-                playa.Precios = new List<PrecioJSON>();
-                foreach (var pre in p.Precios)
-                {
-                    PrecioJSON precio = new PrecioJSON();
-                    precio.Vehiculo = pre.TipoVehiculoStr;
-                    precio.Dia = pre.DiaAtencionStr;
-                    precio.Tiempo = pre.TiempoStr;
-                    precio.Monto = pre.Monto;
-
-                    playa.Precios.Add(precio);
-                }
-
-                playas.Add(playa);
-            }
-            return playas;
+            string playasFiltradas = geoServicio.ObtenerPlayasDeCiudadPorFiltro(ciudadBuscada,tipoPlaya, tipoVehiculo, diaAtencion, precioDesde, precioHasta, horaDesde, horaHasta);
+            return playasFiltradas;
         }
 
     }
