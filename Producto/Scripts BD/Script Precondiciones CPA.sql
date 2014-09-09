f607112016844a9a2d02360e9c8c3c42c2f325c2
leoromero <leonel.romero89@gmail.com>
2014-09-09 18:14:31 -0300

    CP terminados, actualizado el script de precondiciones

--- Producto/Scripts BD/Script Precondiciones CPA.sql
+++ Producto/Scripts BD/Script Precondiciones CPA.sql
@@ -11,12 +11,19 @@ DELETE FROM PlayaDeEstacionamientoes WHERE Nombre LIKE 'CPA_%';
 DELETE FROM DiaAtencions WHERE Nombre LIKE 'CPA_%';
 DELETE FROM Tiempoes WHERE Nombre LIKE 'CPA_%';
 DELETE FROM TipoPlayas WHERE Nombre LIKE 'CPA_%';
+DELETE FROM Ciudads WHERE Nombre LIKE 'CPA_%';
+DELETE FROM Departamentoes WHERE Nombre LIKE 'CPA_%';
+DELETE FROM Permisoes WHERE Nombre LIKE 'CPA_%';
+DELETE FROM Provincias WHERE Nombre LIKE 'CPA_%';
+DELETE FROM Rols WHERE Nombre LIKE 'CPA_%';
 DELETE FROM TipoVehiculoes WHERE Nombre LIKE 'CPA_%';
+
 
 -- Se agregan los datos necesarios para correr los test cases automaticos
 
-INSERT INTO DiaAtencions (Nombre, FechaAlta) VALUES ('CPA_DiasDeAtencion1', '28/08/2014');
-INSERT INTO Tiempoes  (Nombre, FechaAlta) VALUES ('CPA_TipoHorario1', '28/08/2014');
-INSERT INTO TipoPlayas (Nombre, FechaAlta) VALUES ('CPA_TipoPlaya1', '28/08/2014');
-INSERT INTO TipoVehiculoes (Nombre, FechaAlta) VALUES ('CPA_TipoVehiculo1', '28/08/2014');
-INSERT INTO TipoVehiculoes (Nombre, FechaAlta) VALUES ('CPA_TipoVehiculo2', '28/08/2014');
+INSERT INTO DiaAtencions (Nombre, FechaAlta) VALUES ('CPA_DiasDeAtencion1', '2014/08/08');
+INSERT INTO Tiempoes  (Nombre, FechaAlta) VALUES ('CPA_TipoHorario1', '2014/08/08');
+INSERT INTO TipoPlayas (Nombre, FechaAlta) VALUES ('CPA_TipoPlaya1', '2014/08/08');
+INSERT INTO TipoVehiculoes (Nombre, FechaAlta) VALUES ('CPA_TipoVehiculo1', '2014/08/08');
+INSERT INTO TipoVehiculoes (Nombre, FechaAlta) VALUES ('CPA_TipoVehiculo2', '2014/08/08');
+INSERT INTO Permisoes (Nombre, Url, Acceso, FechaAlta) VALUES ('CPA_Permiso', 'CPA_Url', '1', '2014/08/08');
