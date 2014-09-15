fba301e6f6c33c658e613ee45bd5c1266d17b3b0
nachiof <nachiof@gmail.com>
2014-09-14 21:18:25 -0300

    US1
    correccion de que no cargaba los marcadores, y del scroll que aparecía

--- Producto/Movil/GeoParking/www/js/cargarMapa.js
+++ Producto/Movil/GeoParking/www/js/cargarMapa.js
@@ -10,6 +10,7 @@ var contenido = "";
 var infowindow = new google.maps.InfoWindow({
     content: ''
 });
+var lblMensaje = document.getElementById("lblMensaje");
 
 function initialize(){
     
@@ -23,9 +24,11 @@ function initialize(){
     
    
     //crea el mapa en el div "map-canvas" y le setea las opciones
+    
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
    
     //centro el mapa en la posicion actual del movil.
+    
     obtenerPosicionActual();
     
     /*var address = "cordoba, Argentina";
@@ -46,8 +49,8 @@ function addMarker(location) {
     
     var marker = new google.maps.Marker({
         position: location,
-        map: map/*,
-        icon: './img/maracdorParking.png'*/
+        map: map,
+        icon: './img/maracdorParking.png'
     });
     markers.push(marker);
 }
@@ -61,9 +64,6 @@ function showMarkers() {
 }
 function obtenerPosicionActual(){
     var successFunction = function(p){
-        //var lblMensaje = document.getElementById("lblMensaje");
-        //lblMensaje.innerHTML = "Latitud = " + p.coords.latitude + ", Longitud = " + p.coords.longitude;
-        
         if(p.coords.latitude !== undefined && p.coords.longitude !== undefined){
             var posicionGoogle = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
             map.setCenter(posicionGoogle);
@@ -72,9 +72,7 @@ function obtenerPosicionActual(){
                 longitud : p.coords.longitude
             };
             obtenerCiudadDePosicion(posicionInterna);
-            
         }
-
     };
     var errorFunction = function(){
         mensajeErrorConexion();
@@ -82,24 +80,18 @@ function obtenerPosicionActual(){
     intel.xdk.geolocation.getCurrentPosition(successFunction,errorFunction);
 }
 function obtenerCiudadDePosicion(posicion){
-    var lblMensaje = document.getElementById("lblMensaje");
-    lblMensaje.innerHTML = "por generar la uri";
+    //var lblMensaje = document.getElementById("lblMensaje");
+    //lblMensaje.innerHTML = "por generar la uri";
     var uri = "http://maps.googleapis.com/maps/api/geocode/json?latlng="+posicion.latitud+","+posicion.longitud+"&sensor=true";
-    lblMensaje.innerHTML = uri;
     var funcionCiudad = function(direcciones){
         if(direcciones.status === "OK" && direcciones.results.length > 3){
             //en esta posicion siempre se muestra el formato ciudad, provincia, pais
-            var i = direcciones.results.length - 3; 
+            var i = direcciones.results.length - 3;
              //aca tengo la ciudad para llamar al servicio de geoparking que devuelve playas en base a la ciudad
             var ciudad = (direcciones.results[i].formatted_address).split(",")[0];
-            
-            
-            lblMensaje.innerHTML = "Ciudad: " + ciudad;
-            
             var playasDeCiudad = obtenerPlayasDeCiudad(ciudad);
             agregarPlayasAMapa(playasDeCiudad);
             showMarkers();
-            
         }
             
     };
@@ -112,5 +104,5 @@ function agregarPlayasAMapa(playas){
 }
 function agregarPlayaAMapa(playa){
     var posicionDePlaya = new google.maps.LatLng(playa.Latitud, playa.Longitud);
-    addMarker(posicionDePlaya);    
+    addMarker(posicionDePlaya);
 }
