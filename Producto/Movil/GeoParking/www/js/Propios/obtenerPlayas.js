6cafb1663610364f577de7d78b0a84531dc86d7e
nachiof <nachiof@gmail.com>
2014-09-28 22:55:34 -0300

    US30
    se crea el listado y se puede seleccionar una y mostrar el recorrido a
    la misma, tambien se calcula la distancia

--- Producto/Movil/GeoParking/www/js/Propios/obtenerPlayas.js
+++ Producto/Movil/GeoParking/www/js/Propios/obtenerPlayas.js
@@ -1,15 +1,17 @@
 function obtenerPlayasDeCiudad(ciudad){
     if(ciudad === "Capital") ciudad = "cordoba";
-    var uri = 'http://ifrigerio-001-site1.smarterasp.net/api/playas?ciudad=' + ciudad;
+    var uri = 'http://ifrigerio-001-site1.smarterasp.net/api/playas/getPlayas?ciudad=' + ciudad;
+    //var uri = 'http://localhost:5027/api/playas/getPlayas?ciudad=' + ciudad;
     $.ajax({
 			type: "GET",
 			url: uri,
 			success : function(data){
-				var playas = (typeof data) == 'string' ?
+				playas = (typeof data) == 'string' ?
 							   eval('(' + data + ')') :
 							   data;
                 agregarPlayasAMapa(playas);
                 showMarkers();
+                
 			},
 			error: function(jqXHR, textStatus, errorThrown ){
 				alert("ERROR");
