be409b221c72b6981160e90f74d3faceb0a18e1b
leoromero <leonel.romero89@gmail.com>
2014-10-07 15:26:22 -0300

    Test Cases

--- Producto/Scripts BD/Script Precondiciones CPA.sql
+++ Producto/Scripts BD/Script Precondiciones CPA.sql
@@ -1,22 +1,22 @@
 -- Se borran los datos que puedan quedar de otros CPA
 	
 	--Se borrran las relaciones
-DELETE FROM Direccions WHERE PlayaDeEstacionamientoId in (SELECT Id FROM PlayaDeEstacionamientoes p WHERE p.Nombre LIKE 'CPA_%')
-DELETE FROM Horarios WHERE PlayaDeEstacionamientoId in (SELECT Id FROM PlayaDeEstacionamientoes p WHERE p.Nombre LIKE 'CPA_%')
-DELETE FROM Precios WHERE PlayaDeEstacionamientoId in (SELECT Id FROM PlayaDeEstacionamientoes p WHERE p.Nombre LIKE 'CPA_%')
-DELETE FROM Servicios WHERE PlayaDeEstacionamientoId in (SELECT Id FROM PlayaDeEstacionamientoes p WHERE p.Nombre LIKE 'CPA_%')
+DELETE FROM Direccions;
+DELETE FROM Horarios;
+DELETE FROM Precios;
+DELETE FROM Servicios;
 	
 	--Se borran los datos
-DELETE FROM PlayaDeEstacionamientoes WHERE Nombre LIKE 'CPA_%';
-DELETE FROM DiaAtencions WHERE Nombre LIKE 'CPA_%';
-DELETE FROM Tiempoes WHERE Nombre LIKE 'CPA_%';
-DELETE FROM TipoPlayas WHERE Nombre LIKE 'CPA_%';
+DELETE FROM PlayaDeEstacionamientoes;
+DELETE FROM DiaAtencions;
+DELETE FROM Tiempoes;
+DELETE FROM TipoPlayas;
 DELETE FROM Ciudads WHERE Nombre LIKE 'CPA_%';
 DELETE FROM Departamentoes WHERE Nombre LIKE 'CPA_%';
 DELETE FROM Permisoes WHERE Nombre LIKE 'CPA_%';
 DELETE FROM Provincias WHERE Nombre LIKE 'CPA_%';
-DELETE FROM Rols WHERE Nombre LIKE 'CPA_%';
-DELETE FROM TipoVehiculoes WHERE Nombre LIKE 'CPA_%';
+DELETE FROM Rols  WHERE Nombre LIKE 'CPA_%';
+DELETE FROM TipoVehiculoes;
 
 
 -- Se agregan los datos necesarios para correr los test cases automaticos
