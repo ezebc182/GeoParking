dd3cbc94d098f8246aea2cb4ce29d07454e6aea1
leoromero <leonel.romero89@gmail.com>
2014-10-21 10:20:07 -0300

    Estadisticas

---
+++ Producto/Web/Codigo/Aplicacion/Web/DensidadConsultasPorFechas.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ReglasDeNegocio;
+using System.Web.Services;
+
+namespace Web
+{
+    public partial class DensidadConsultasPorFechas : System.Web.UI.Page
+    {
+        //referencia a la master
+        public static SiteMaster master;
+
+        //gestor de busqueda de playas
+        private static GestorBusquedaPlayas gestor;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            master = (SiteMaster)Master.Master;
+
+            gestor = new GestorBusquedaPlayas();
+
+            if (!Page.IsPostBack)
+            {
+
+               
+            }
+
+        }
+
+        /// <summary>
+        /// Buscar las playas de estacionamiento de la ciudad seleccionada en el Index
+        /// </summary>
+        /// <returns>Lista de playas de la ciudad</returns>
+        [WebMethod]
+        public static string ObtenerPlayasDeCiudad()
+        {
+            var playas = gestor.buscarPlayasPorCiudad("Cordoba");
+
+            string json = "[";
+
+            foreach (var p in playas)
+            {
+                json += p.ToJSONRepresentation() + ",";
+            }
+
+            if (playas.Count != 0)
+            {
+                json = json.Substring(0, json.Length - 1);
+            }
+            json += "]";
+
+            if (json == "[]")
+            {
+                master.MostrarMensajeInformacion("No hay resultados para la ciudad seleccionada", "Resultado Busqueda");
+            }
+            return json;
+        }
+
+
+    }
+}
---
+++ Producto/Web/Codigo/Aplicacion/Web/Scripts/GoogleMapsEstadisticas.js
@@ -0,0 +1,91 @@
+﻿//mapas de la pagina
+var maps = new Array;
+//array de marcadores
+var markers = new Array();
+//array de datos
+var pointArray = new Array();
+//array de heatmaps
+var heatmap = new Array();
+//array de los valores de los range
+var min = new Array();
+
+function initialize(IdMapa) {
+
+    //variable para la busqueda con una direccion
+    geocoder = new google.maps.Geocoder();
+
+    //centrar el mapa en la plaza San Martin
+    var puntoPlaza = new google.maps.LatLng(-31.416756, -64.183501);
+
+    //opciones basicas del mapa
+    var mapOptions = {
+        zoom: 12,
+        center: puntoPlaza,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    //crea el mapa en el div "map-canvas" y le setea las opciones
+    maps.push(new google.maps.Map(document.getElementById('map-' + IdMapa),
+        mapOptions));
+
+    //Se agrega un array de marcadores para usar con el mapa actual, un array de puntos, y uno de heatmaps
+    if (markers.length !== IdMapa - 1) {
+        markers.push(new Array());
+        pointArray.push(new Array());
+        heatmap.push(new Array());
+    }
+
+    calcularIdHM(IdMapa);
+    //recuperamos las playas de la ciudad
+    getPlayas(IdMapa, IdHM);
+}
+
+//AGREGAR MARCADORES DE LAS PLAYAS DE LA CIUDAD BUSCADA EN EL INDEX
+function getPlayas(IdMapa, IdHM) {
+
+    //peticionAjax
+    $.ajax({
+        type: "POST",
+        url: "DensidadConsultasPorFechas.aspx/ObtenerPlayasDeCiudad",
+        data: "{}",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            var playasArray = (typeof response.d) == 'string' ?
+                           eval('(' + response.d + ')') :
+                           response.d;
+
+            var playas = new Array();
+
+            for (i = 0; i < playasArray.length; i++) {
+
+                playas.push(new google.maps.LatLng(playasArray[i].Latitud, playasArray[i].Longitud));
+            }
+            markers[IdMapa][IdHM] = playas;
+            cargarHeatMap(IdMapa, IdHM);
+        },
+        error: function (result) {
+            Alerta_openModalError('ERROR ' + result.status + ' ' + result.statusText, 'Error');
+        }
+    });
+}
+function cargarHeatMap(IdMapa, IdHM) {
+
+    pointArray[IdMapa][IdHM] = new google.maps.MVCArray(markers[IdMapa][IdHM]);
+
+    heatmap[IdMapa][IdHM] = new google.maps.visualization.HeatmapLayer({
+        data: pointArray[IdMapa][IdHM],
+        map: maps[IdMapa]
+    });
+}
+function calcularIdHM(IdMapa) {
+    //Se setea el mes elegido del mapa
+    ano = $('[id*=txtMes-' + IdMapa + ']').val();
+    min[IdMapa] = $('[id*=txtMes-' + IdMapa + ']').attr('min');
+    IdHM = ano - min
+}
+
+function toggleHeatmap(IdMapa) {
+    calcularIdHM(IdMapa);
+    heatmap[IdMapa][IdHM].setMap(heatmap[Id][IdHM].getMap() ? null : maps[IdMapa]);
+}
