18a3209c49549cd10c4da610509e20145a9bf4fc
lucastoneatto <lucastoneatto@gmail.com>
2014-12-27 19:12:17 -0300

    Creacion del controller para actualizacion de servicios
    permite la creacion y la baja de un servicio de una playa de
    estacionamiento

--- Producto/Web/Codigo/Aplicacion/Negocio/ReglasDeNegocio/GestorServicio.cs
+++ Producto/Web/Codigo/Aplicacion/Negocio/ReglasDeNegocio/GestorServicio.cs
@@ -40,8 +40,8 @@ namespace ReglasDeNegocio
         /// <summary>
         /// Busca un tipo de vehiculo por su id
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">id del tipo de vehiculo</param>
+        /// <returns>tipo de vehiculo segun su id</returns>
         public TipoVehiculo BuscarTipoVehiculoPorId(int id)
         {
             return tipoVehiculoDao.FindById(id);
@@ -49,11 +49,128 @@ namespace ReglasDeNegocio
         /// <summary>
         /// Busca todos los tipos de vehiculos
         /// </summary>
-        /// <returns></returns>
+        /// <returns>lista de tipos de vehiculos</returns>
         public IList<TipoVehiculo> BuscarTipoVehiculos()
         {
             return tipoVehiculoDao.FindAll();
         }
 
+        /// <summary>
+        /// Cancela y da de baja el servicio de la playa de estacionamiento.
+        /// </summary>
+        /// <param name="idPlaya">id de la playa</param>
+        /// <param name="idTipoVechiculo">id de tipo de vehiculo</param>
+        /// <returns>'True' si la operacion se realizo correctamente</returns>
+        public Resultado CancelarServicioPlaya(int idPlaya, int idTipoVechiculo)
+        {
+            Resultado resultado = new Resultado();
+
+            try
+            {
+                Servicio servicioRecuperado = servicioDao.FindWhere(s => s.PlayaDeEstacionamientoId == idPlaya && s.TipoVehiculoId == idTipoVechiculo).First();
+
+                servicioRecuperado.FechaBaja = DateTime.Now;
+
+                servicioDao.Update(servicioRecuperado);
+            }
+            catch (Exception)
+            {
+                resultado.AgregarMensaje("Se ha producido un error de base de datos.");
+            }            
+
+            return resultado;
+        }
+
+        /// <summary>
+        /// Registro de un nuevo servicio
+        /// </summary>
+        /// <param name="idPlaya">id de la playa</param>
+        /// <param name="idTipoVechiculo">id del tipo de vehiculo</param>
+        /// <param name="capacidad">capacidad (lugares para el tipo de vehiculo)</param>
+        /// <param name="x1">precio por hora</param>
+        /// <param name="x6">precio por 6 horas</param>
+        /// <param name="x12">precio por 12 horas</param>
+        /// <param name="x24">precio por 24 horas</param>
+        /// <param name="abono">precio por mes</param>
+        /// <returns>'True' si la operacion se realizo correctamente</returns>
+        public Resultado RegistrarServicioPlaya(int idPlaya, int idTipoVechiculo, int capacidad, double x1, double x6, double x12, double x24, double abono)
+        {
+            Resultado resultado = new Resultado();
+
+            try
+            {
+
+                Servicio servicio = new Servicio();
+                servicio.PlayaDeEstacionamientoId = idPlaya;
+                servicio.TipoVehiculoId = idTipoVechiculo;
+                
+                //capacidad
+                Capacidad cap = new Capacidad();
+                cap.Cantidad=capacidad;
+                servicio.Capacidad = cap;
+
+                //precios
+                List<Precio> precios = new List<Precio>();
+
+                //por hora
+                if (x1 != 0)
+                {
+                    Precio p1 = new Precio();
+                    p1.TiempoId = 1;
+                    p1.Monto = Decimal.Parse(x1.ToString());
+
+                    precios.Add(p1);
+                }
+
+                //por 6 horas
+                if (x6 != 0)
+                {
+                    Precio p2 = new Precio();
+                    p2.TiempoId = 2;
+                    p2.Monto = Decimal.Parse(x6.ToString());
+
+                    precios.Add(p2);
+                }
+
+                //por 12 horas
+                if (x12 != 0)
+                {
+                    Precio p3 = new Precio();
+                    p3.TiempoId = 3;
+                    p3.Monto = Decimal.Parse(x12.ToString());
+
+                    precios.Add(p3);
+                }
+
+                //por 24 horas
+                if (x24 != 0)
+                {
+                    Precio p4 = new Precio();
+                    p4.TiempoId = 1;
+                    p4.Monto = Decimal.Parse(x24.ToString());
+
+                    precios.Add(p4);
+                }
+
+                //abono mensual
+                if (abono != 0)
+                {
+                    Precio p5 = new Precio();
+                    p5.TiempoId = 1;
+                    p5.Monto = Decimal.Parse(abono.ToString());
+
+                    precios.Add(p5);
+                }
+
+                //creacion del servicio
+                servicioDao.Create(servicio);
+            }
+            catch (Exception)
+            {
+                resultado.AgregarMensaje("Se ha producido un error de base de datos.");
+            }
+
+            return resultado;
+        }
     }
 }
---
+++ Producto/Web/Codigo/Aplicacion/Negocio/WebServiceGeo/WebServiceGeo/Controllers/ServiciosController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ReglasDeNegocio;
+
+namespace WebServiceGeo.Controllers
+{
+    public class ServiciosController : ApiController
+    {
+        GestorServicio gestor = new GestorServicio();
+
+        // GET api/servicios
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// cancela o da de baja el servicio
+        /// </summary>
+        /// <param name="idPlaya">id de la playa</param>
+        /// <param name="idTipoVechiculo">id del tipo de vehiculo</param>
+        /// <returns>'True' si la operacion se realizo correctamente</returns>
+        public string GetCancelarServicio([FromUri]int idPlaya, [FromUri]int idTipoVechiculo)
+        {
+            return gestor.CancelarServicioPlaya(idPlaya, idTipoVechiculo).Ok.ToString();
+        }
+
+        /// <summary>
+        /// registra un nuevo servicio de la playa
+        /// </summary>
+        /// <param name="idPlaya">id de la playa</param>
+        /// <param name="idTipoVechiculo">id del tipo de vehiculo</param>
+        /// <param name="capacidad">capacidad(lugares para el tipo de vehiculo)</param>
+        /// <param name="x1">precio por hora</param>
+        /// <param name="x6">precio por 6 horas</param>
+        /// <param name="x12">precio por 12 horas</param>
+        /// <param name="x24">precio por 24 horas</param>
+        /// <param name="abono">precio por mes</param>
+        /// <returns></returns>
+        public string getRegistrarServicio([FromUri]int idPlaya, [FromUri]int idTipoVechiculo, [FromUri]int capacidad, [FromUri]double x1, [FromUri]double x6, [FromUri]double x12, [FromUri]double x24, [FromUri]double abono)
+        {
+            return gestor.RegistrarServicioPlaya( idPlaya,  idTipoVechiculo,  capacidad,  x1, x6, x12, x24, abono).Ok.ToString();
+        }
+
+        // GET api/servicios/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/servicios
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/servicios/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/servicios/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
