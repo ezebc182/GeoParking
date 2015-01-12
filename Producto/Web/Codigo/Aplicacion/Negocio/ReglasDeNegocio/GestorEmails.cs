25acb16ba5ded05788d6b013146646bc39b42139
nachiof <nachiof@gmail.com>
2015-01-12 19:24:07 -0300

    Servicio y gestor de envio de emails

---
+++ Producto/Web/Codigo/Aplicacion/Negocio/ReglasDeNegocio/GestorEmails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReglasDeNegocio
+{
+    public class GestorEmails
+    {
+        public bool EnviarEmail(string contenidoMensaje)
+        {
+            try
+            {
+                System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
+                message.To.Add("info.geoparking@gmail.com");
+                message.Subject = "GeoParking - Contacto";
+                message.From = new System.Net.Mail.MailAddress("contacto@geoparking.com");
+                message.Body = contenidoMensaje;
+                System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587);
+                NetworkCredential basicCredential = new NetworkCredential("info.geoparking", "geoparking5");
+                smtp.Credentials = basicCredential;
+                smtp.EnableSsl = true;
+                smtp.Send(message);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Producto/Web/Codigo/Aplicacion/Negocio/WebServiceGeo/WebServiceGeo/Controllers/ContactoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using ReglasDeNegocio;
+
+namespace WebServiceGeo.Controllers
+{
+    public class Contacto
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Mensaje { get; set; }
+        
+    }
+    public class ContactoController : ApiController
+    {
+        GestorEmails emails = new GestorEmails();
+        [System.Web.Mvc.HttpPost]
+        public void PostEnviarEmailDeContacto([FromBody] Contacto mensaje)
+        {
+            string contenidoMensaje = "Nombre: " + mensaje.Nombre;
+            contenidoMensaje += "\nApellido: " + mensaje.Apellido;
+            contenidoMensaje += "\nTelefono: " + mensaje.Telefono;
+            contenidoMensaje += "\nEmail: " + mensaje.Email;
+            contenidoMensaje += "\nCosulta:\n" + mensaje.Mensaje;
+            emails.EnviarEmail(contenidoMensaje).ToString();
+        }
+
+    }
+}
