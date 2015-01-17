8f4f3e1b025c5f2b46d1d3ca89c906cb1ebe5151
nachiof <nachiof@gmail.com>
2015-01-16 23:24:20 -0300

    Elimino codigo q commitie sin querer
    era de unua prueba q estaba haciendo

--- Producto/Web/Codigo/Aplicacion/Negocio/WebServiceGeo/WebServiceGeo/Controllers/ContactoController.cs
+++ Producto/Web/Codigo/Aplicacion/Negocio/WebServiceGeo/WebServiceGeo/Controllers/ContactoController.cs
@@ -27,32 +27,12 @@ namespace WebServiceGeo.Controllers
         [System.Web.Mvc.HttpPost]
         public void PostEnviarEmailDeContacto([FromBody] Contacto mensaje)
         {
-            var headers = Request.Headers;
-            System.Web.HttpContext.Current.Response.Clear();
-            try
-            {
-                //IEnumerable<string> headerPasswordValues = Request.Headers.GetValues("x-password");
-                string token = Request.Headers.GetValues("x-token").FirstOrDefault();
-                if (token.CompareTo("eqwadasdasd") != 0)//aca es donde se validaria el token con el almacenado en el servidor
-                {
-                    throw new HttpRequestException("No posee acceso al sistema");
-                }
-                string contenidoMensaje = "Nombre: " + mensaje.Nombre;
-                contenidoMensaje += "\nApellido: " + mensaje.Apellido;
-                contenidoMensaje += "\nTelefono: " + mensaje.Telefono;
-                contenidoMensaje += "\nEmail: " + mensaje.Email;
-                contenidoMensaje += "\nCosulta:\n" + mensaje.Mensaje;
-                emails.EnviarEmail(contenidoMensaje).ToString();
-            }
-            catch (HttpRequestException)
-            {
-                System.Web.HttpContext.Current.Response.StatusCode = 403;
-                //throw new Exception("No posee acceso al sistema");
-            }
-            catch (Exception)
-            {
-                System.Web.HttpContext.Current.Response.StatusCode = 400;
-            }
+            string contenidoMensaje = "Nombre: " + mensaje.Nombre;
+            contenidoMensaje += "\nApellido: " + mensaje.Apellido;
+            contenidoMensaje += "\nTelefono: " + mensaje.Telefono;
+            contenidoMensaje += "\nEmail: " + mensaje.Email;
+            contenidoMensaje += "\nCosulta:\n" + mensaje.Mensaje;
+            emails.EnviarEmail(contenidoMensaje).ToString();
         }
 
     }
