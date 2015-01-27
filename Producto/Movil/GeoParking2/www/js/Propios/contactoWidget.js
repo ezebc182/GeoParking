a8f66e3a6049483f2245b79d9a32e1ebb39e5dbe
Ezequiel <eebarcoch@gmail.com>
2015-01-27 02:33:35 -0300

    Movil: Listado de historico de consultas de playa
    Algunos arreglos de interfaz

--- Producto/Movil/GeoParking2/www/js/Propios/contactoWidget.js
+++ Producto/Movil/GeoParking2/www/js/Propios/contactoWidget.js
@@ -1,120 +1,114 @@
-$.widget( "geoparking.contactoWidget", {
-	options : {
-		nombre : null,
-		apellido : null,
-		telefono : null,
-		email : null,
-		mensaje : null,
-		contenedor : $("#panelContacto")
-	},
-	_create : function(){
-		var widget = this;
+$.widget("geoparking.contactoWidget", {
+    options: {
+        nombre: null,
+        apellido: null,
+        telefono: null,
+        email: null,
+        mensaje: null,
+        contenedor: $("#panelContacto")
+    },
+    _create: function () {
+        var widget = this;
         $("#panelConfiguraciones").preferenciasWidget("destroy");
-	},
+    },
     /**
-    * Verifica si ya existe o el panel de contactos y lo crea o elimina 
-    * segun corresponda, se ejecuta cada vez que se hace la llamadad $("algun selector").contactoWidget();
-    */
-	_init : function(){
-		var widget = this;
-		if(widget.options.contenedor.children().length === 0){
+     * Verifica si ya existe o el panel de contactos y lo crea o elimina
+     * segun corresponda, se ejecuta cada vez que se hace la llamadad $("algun selector").contactoWidget();
+     */
+    _init: function () {
+        var widget = this;
+        if (widget.options.contenedor.children().length === 0) {
             $("#pnlMapa").hide();
-			widget._crearHTMLInicial();
+            widget._crearHTMLInicial();
             $("a[href=#mypanel]").click();
-		}
-		else{
-			widget.destroy();
+        } else {
+            widget.destroy();
             $("a[href=#mypanel]").click();
-		}
-	},
+        }
+    },
     /**
-    * Se destruye el widget limpiando todos los componentes que creo
-    */
-    destroy : function(){
+     * Se destruye el widget limpiando todos los componentes que creo
+     */
+    destroy: function () {
         var widget = this;
         widget.options.contenedor.empty();
         $("#pnlMapa").show();
         $.Widget.prototype.destroy.call(this);
     },
     /**
-    * Crea la estructura inicial del panel de contacto
-    */
-	_crearHTMLInicial : function () {
-		var widget = this;
-        widget.options.contenedor.append('<div data-role="content" class="ui-content" role="main"><form data-ajax="false"><ul data-role="listview" data-inset="true" class="ui-listview ui-listview-inset ui-corner-all ui-shadow" id="listadoConfiguraciones" ><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="nombre">Nombre:</label><div class="ui-input-text ui-body-inherit ui-corner-all ui-shadow-inset ui-input-has-clear"><input type="text" name="nombre" id="nombre" value="" data-clear-btn="true" placeholder="Ingrese su nombre aqui"><a href="#" class="ui-input-clear ui-btn ui-icon-delete ui-btn-icon-notext ui-corner-all ui-input-clear-hidden" title="Clear text">Borrar nombre</a></div></li><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="apellido">Apellido:</label><div class="ui-input-text ui-body-inherit ui-corner-all ui-shadow-inset ui-input-has-clear"><input type="text" name="apellido" id="apellido" value="" data-clear-btn="true" placeholder="Ingrese su apellido aqui"><a href="#" class="ui-input-clear ui-btn ui-icon-delete ui-btn-icon-notext ui-corner-all ui-input-clear-hidden" title="Clear text">Borrar apellido</a></div></li><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="telefono">Teléfono:</label><div class="ui-input-text ui-body-inherit ui-corner-all ui-shadow-inset ui-input-has-clear"><input type="text" name="telefono" id="telefono" value="" data-clear-btn="true" placeholder="Ingrese su telefono aqui"><a href="#" class="ui-input-clear ui-btn ui-icon-delete ui-btn-icon-notext ui-corner-all ui-input-clear-hidden" title="Clear text">Borrar teléfono</a></div></li><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="inputEmail">Ingrese su email:</label><div class="ui-input-text ui-body-inherit ui-corner-all ui-shadow-inset ui-input-has-clear"><input type="email" name="inputEmail" id="inputEmail" value="" data-clear-btn="true" placeholder=""><a href="#" class="ui-input-clear ui-btn ui-icon-delete ui-btn-icon-notext ui-corner-all ui-input-clear-hidden" title="Clear text">Borrar email</a></div></li><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="mensaje">Textarea:</label><textarea cols="40" rows="8" name="mensaje" id="mensaje" placeholder="Mensaje, duda o consulta" class="ui-input-text ui-shadow-inset ui-body-inherit ui-corner-all ui-textinput-autogrow" style="height: 53px;"></textarea></li><li class="ui-li-static ui-body-inherit ui-last-child"><div class="ui-btn ui-input-btn ui-corner-all ui-shadow">Enviar<input id="btnEnviarContacto" type="submit" value="Guardar"></div><div class="ui-btn ui-input-btn ui-corner-all ui-shadow">Cancelar<input id="btnCancelarContacto" type="submit" value="Cancelar"></div></li></ul></form></div>');
-        $("#btnEnviarContacto").click(function(){
-			$("#panelContacto").contactoWidget("tomarDatosFormulario");
-		});
-        var loaderOff = function(){
-            $.mobile.loading( "hide" );
+     * Crea la estructura inicial del panel de contacto
+     */
+    _crearHTMLInicial: function () {
+        var widget = this;
+        widget.options.contenedor.append('<div data-role="content" class="ui-content" role="main"><form data-ajax="false"><ul data-role="listview" data-inset="true" class="ui-listview ui-listview-inset ui-corner-all ui-shadow" id="listadoConfiguraciones" ><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="nombre">Nombre:</label><div class="ui-input-text ui-body-inherit ui-corner-all ui-shadow-inset ui-input-has-clear"><input type="text" name="nombre" id="nombre" value="" data-clear-btn="true" placeholder="Ingrese su nombre aqui"><a href="#" class="ui-input-clear ui-btn ui-icon-delete ui-btn-icon-notext ui-corner-all ui-input-clear-hidden" title="Clear text">Borrar nombre</a></div></li><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="apellido">Apellido:</label><div class="ui-input-text ui-body-inherit ui-corner-all ui-shadow-inset ui-input-has-clear"><input type="text" name="apellido" id="apellido" value="" data-clear-btn="true" placeholder="Ingrese su apellido aqui"><a href="#" class="ui-input-clear ui-btn ui-icon-delete ui-btn-icon-notext ui-corner-all ui-input-clear-hidden" title="Clear text">Borrar apellido</a></div></li><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="telefono">Teléfono:</label><div class="ui-input-text ui-body-inherit ui-corner-all ui-shadow-inset ui-input-has-clear"><input type="text" name="telefono" id="telefono" value="" data-clear-btn="true" placeholder="Ingrese su telefono aqui"><a href="#" class="ui-input-clear ui-btn ui-icon-delete ui-btn-icon-notext ui-corner-all ui-input-clear-hidden" title="Clear text">Borrar teléfono</a></div></li><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="inputEmail">Ingrese su email:</label><div class="ui-input-text ui-body-inherit ui-corner-all ui-shadow-inset ui-input-has-clear"><input type="email" name="inputEmail" id="inputEmail" value="" data-clear-btn="true" placeholder=""><a href="#" class="ui-input-clear ui-btn ui-icon-delete ui-btn-icon-notext ui-corner-all ui-input-clear-hidden" title="Clear text">Borrar email</a></div></li><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="mensaje">Mensaje:</label><textarea cols="40" rows="8" name="mensaje" id="mensaje" placeholder="Mensaje, duda o consulta" class="ui-input-text ui-shadow-inset ui-body-inherit ui-corner-all ui-textinput-autogrow" style="height: 53px;"></textarea></li><li class="ui-li-static ui-body-inherit ui-last-child"><div class="ui-btn ui-input-btn ui-corner-all ui-shadow">Enviar<input id="btnEnviarContacto" type="submit" value="Guardar"></div><div class="ui-btn ui-input-btn ui-corner-all ui-shadow">Cancelar<input id="btnCancelarContacto" type="submit" value="Cancelar"></div></li></ul></form></div>');
+        $("#btnEnviarContacto").click(function () {
+            $("#panelContacto").contactoWidget("tomarDatosFormulario");
+        });
+        var loaderOff = function () {
+            $.mobile.loading("hide");
         };
-        setTimeout(loaderOff,300);
-	},
+        setTimeout(loaderOff, 300);
+    },
     /**
-    * Toma los datos del formulario para enviarlos por mail
-    * verificando que los mismos sean correctos
-    */
-	tomarDatosFormulario : function(){
-		var widget = this;
-        if(widget._validarValoresDeControles()){
+     * Toma los datos del formulario para enviarlos por mail
+     * verificando que los mismos sean correctos
+     */
+    tomarDatosFormulario: function () {
+        var widget = this;
+        if (widget._validarValoresDeControles()) {
             var datos = {
-                Nombre : $("#nombre").val(),
-                Apellido : $("#apellido").val(),
-                Telefono : $("#telefono").val(),
-                Email : $("#inputEmail").val(),
-                Mensaje : $("#mensaje").val()
+                Nombre: $("#nombre").val(),
+                Apellido: $("#apellido").val(),
+                Telefono: $("#telefono").val(),
+                Email: $("#inputEmail").val(),
+                Mensaje: $("#mensaje").val()
             };
             widget._enviarFormulario(datos);
         }
-	},
+    },
     /**
-    * Se valida que se hayan ingresado todos los campos
-    * y se muestra el mensaje correspondiente si no 
-    * se hizo de la manera correcta
-    */
-    _validarValoresDeControles : function(){
+     * Se valida que se hayan ingresado todos los campos
+     * y se muestra el mensaje correspondiente si no
+     * se hizo de la manera correcta
+     */
+    _validarValoresDeControles: function () {
         var widget = this;
-        if($("#nombre").val() === ""){
+        if ($("#nombre").val() === "") {
             widget._abrirPopup("Debe ingresar un nombre");
-        }
-        else if($("#apellido").val() === ""){
+        } else if ($("#apellido").val() === "") {
             widget._abrirPopup("Debe ingresar un apellido");
-        }
-        else if(!validarNumberoTelefono($("#telefono")[0])){
+        } else if (!validarNumberoTelefono($("#telefono")[0])) {
             widget._abrirPopup("Debe ingresar un numero de telefono correcto");
             return false;
-        }
-        else if(!validarEmail($("#inputEmail")[0])){
+        } else if (!validarEmail($("#inputEmail")[0])) {
             widget._abrirPopup("Debe ingresar un email correcto");
-            return false; 
-        }
-        else if($("#mensaje").val() === ""){
+            return false;
+        } else if ($("#mensaje").val() === "") {
             widget._abrirPopup("El mensaje debe tener contenido");
-            return false; 
-        }
-        else {
+            return false;
+        } else {
             return true;
         }
     },
     /**
-    * Envia los datos recibidos por mail (a traves del servidor)
-    */
-	_enviarFormulario : function(datos){
-		var widget = this;
-		var uri = obtenerURLServer() + "api/Contacto/PostEnviarEmailDeContacto";
-		$.ajax({
-			type: "POST",
-			url: uri,
-			data: datos,
-			dataType: "json",
-			content: "application/json; charset=utf-8",
-			success: function(){
-				alert("se envio la consulta");
+     * Envia los datos recibidos por mail (a traves del servidor)
+     */
+    _enviarFormulario: function (datos) {
+        var widget = this;
+        var uri = obtenerURLServer() + "api/Contacto/PostEnviarEmailDeContacto";
+        $.ajax({
+            type: "POST",
+            url: uri,
+            data: datos,
+            dataType: "json",
+            content: "application/json; charset=utf-8",
+            success: function () {
+                alert("se envio la consulta");
                 widget.destroy();
-			},
-		});
-	},
-    _abrirPopup : function(mensaje){
+            },
+        });
+    },
+    _abrirPopup: function (mensaje) {
         abrirPopup(mensaje)
         /*var widget = this;
         var divPopup = document.createElement("div");
--- Producto/Movil/GeoParking2/www/js/Propios/preferencesWidget.js
+++ Producto/Movil/GeoParking2/www/js/Propios/preferencesWidget.js
@@ -1,32 +1,31 @@
-$.widget( "geoparking.preferenciasWidget", {
-	options : {
-		tipoVehiculoId : null,
-		radio : null,
-		gpsOnOff : null,
-		contenedor : $("#panelConfiguraciones")
-	},
-	_create : function(){
-		var widget = this;
+$.widget("geoparking.preferenciasWidget", {
+    options: {
+        tipoVehiculoId: null,
+        radio: null,
+        gpsOnOff: null,
+        contenedor: $("#panelConfiguraciones")
+    },
+    _create: function () {
+        var widget = this;
         $("#panelContacto").contactoWidget("destroy");
-	},
+    },
     /**
-    * Verifica si ya existe o el panel de configuraciones y lo crea o elimina 
-    * segun corresponda, se ejecuta cada vez que se hace la llamadad $("algun selector").preferenciasWidget();
-    */
-	_init : function(){
-		var widget = this;
-		if(widget.options.contenedor.children().length === 0){
+     * Verifica si ya existe o el panel de configuraciones y lo crea o elimina
+     * segun corresponda, se ejecuta cada vez que se hace la llamadad $("algun selector").preferenciasWidget();
+     */
+    _init: function () {
+        var widget = this;
+        if (widget.options.contenedor.children().length === 0) {
             $("#pnlMapa").hide();
-			widget._crearHTMLInicial();
-			widget._agregarTiposDeVehiculos();
-			widget._cargarValoresAlmacenados();
+            widget._crearHTMLInicial();
+            widget._agregarTiposDeVehiculos();
+            widget._cargarValoresAlmacenados();
             $("a[href=#mypanel]").click();
-		}
-		else{
-			widget.destroy();
-		}
-	},
-    destroy : function(){
+        } else {
+            widget.destroy();
+        }
+    },
+    destroy: function () {
         var widget = this;
         widget.options.contenedor.empty();
         $("#pnlMapa").show();
@@ -34,92 +33,91 @@ $.widget( "geoparking.preferenciasWidget", {
         $.Widget.prototype.destroy.call(this);
     },
     /**
-    * Crea la estructura inicial de la pantalla de configuraciones
-    */
-	_crearHTMLInicial : function () {
-		var widget = this;
-        widget.options.contenedor.append('<div data-role="content" class="ui-content" role="main"><form data-ajax="false"><ul data-role="listview" data-inset="true" class="ui-listview ui-listview-inset ui-corner-all ui-shadow" id="listadoConfiguraciones" ><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="select-choice-1b" class="select">Select</label><div class="ui-select"><select name="select-choice-1" id="tipoVehiculoSelect" data-native-menu="true"><option value="0">Seleccione</option></select></div></li><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="slider2b" id="slider2b-label">Radio:</label><input type="number" data-type="range" name="slider2" id="slider2b" value="100" min="100" max="1000" data-highlight="true" class="ui-shadow-inset ui-body-inherit ui-corner-all ui-slider-input"></li><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="checkbox-based-flipswitch">GPS:</label><input type="checkbox" id="checkbox-based-flipswitch" data-role="flipswitch"></li><li class="ui-li-static ui-body-inherit ui-last-child"><div class="ui-btn ui-input-btn ui-corner-all ui-shadow">Guardar<input id="btnGuardarConfig" type="submit" value="Guardar"></div><div class="ui-btn ui-input-btn ui-corner-all ui-shadow">Cancelar<input id="btnCancelarConfig" type="submit" value="Cancelar"></div></li></ul></form></div>');
+     * Crea la estructura inicial de la pantalla de configuraciones
+     */
+    _crearHTMLInicial: function () {
+        var widget = this;
+        widget.options.contenedor.append('<div data-role="content" class="ui-content" role="main"><form data-ajax="false"><ul data-role="listview" data-inset="true" class="ui-listview ui-listview-inset ui-corner-all ui-shadow" id="listadoConfiguraciones" ><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="select-choice-1b" class="select">Su vehiculo:</label><div class="ui-select"><select name="select-choice-1" id="tipoVehiculoSelect" data-native-menu="true"><option value="0">Seleccione</option></select></div></li><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="slider2b" id="slider2b-label">Radio de búsqueda:</label><input type="number" data-type="range" name="slider2" id="slider2b" value="100" min="100" max="2000" data-highlight="true" class="ui-shadow-inset ui-body-inherit ui-corner-all ui-slider-input">Metros</li><li data-role="fieldcontain" class="ui-field-contain ui-li-static ui-body-inherit"><label for="checkbox-based-flipswitch">GPS:</label><input type="checkbox" id="checkbox-based-flipswitch" data-role="flipswitch"></li><li class="ui-li-static ui-body-inherit ui-last-child"><div class="ui-btn ui-input-btn ui-corner-all ui-shadow">Guardar<input id="btnGuardarConfig" type="submit" value="Guardar"></div><div class="ui-btn ui-input-btn ui-corner-all ui-shadow">Cancelar<input id="btnCancelarConfig" type="submit" value="Cancelar"></div></li></ul></form></div>');
         widget._incializarWindgetDeControles();
-	},
+    },
     /**
-    * incializa los winget de los controles y los reubica dentro del listado de controles
-    */
-    _incializarWindgetDeControles : function(){
+     * incializa los winget de los controles y los reubica dentro del listado de controles
+     */
+    _incializarWindgetDeControles: function () {
         $('select').selectmenu();
         $(" input[type=number]").slider();
         $(" input[type=checkbox]").flipswitch();
-        
-        $("#btnGuardarConfig").parent().click(function(){
+
+        $("#btnGuardarConfig").parent().click(function () {
             $("#panelConfiguraciones").preferenciasWidget("guardarValoresSeleccionados");
         });
-        $("#btnCancelarConfig").parent().click(function(){
+        $("#btnCancelarConfig").parent().click(function () {
             $("#panelConfiguraciones").preferenciasWidget("destroy");
         });
-        
+
         $("#listadoConfiguraciones").append($(" input[type=number]").parent().parent());
         $("#listadoConfiguraciones").append($(" label[for=checkbox-based-flipswitch]").parent());
         $("#listadoConfiguraciones").append($(" button[type=submit]").parent());
         $("#listadoConfiguraciones").append($("#btnGuardarConfig").parent().parent());
     },
     /**
-    * Toma los valores del local storage y setea los valores de los controles
-    */
-	_cargarValoresAlmacenados : function () {
-		var widget = this;
-		widget.options.radio = leerPropiedadRadio();
-		widget.options.tipoVehiculoId = leerPropiedadTipoVehiculo();
+     * Toma los valores del local storage y setea los valores de los controles
+     */
+    _cargarValoresAlmacenados: function () {
+        var widget = this;
+        widget.options.radio = leerPropiedadRadio();
+        widget.options.tipoVehiculoId = leerPropiedadTipoVehiculo();
         widget.options.gpsOnOff = leerPropiedadGPS();
-		$('#tipoVehiculoSelect').val(widget.options.tipoVehiculoId);
+        $('#tipoVehiculoSelect').val(widget.options.tipoVehiculoId);
         $("#tipoVehiculoSelect-button > span").html($('select option:selected').html());
         $(" input[type=number]").val(widget.options.radio);
         var porcentajeBarra = ((widget.options.radio - 100) / 900) * 100;
-        $($(" input[type=number]").parent().children()[1]).children()[0].style.width = porcentajeBarra +"%";
-        $($(" input[type=number]").parent().children()[1]).children()[1].style.left = porcentajeBarra +"%";
-        $($($(" input[type=number]").parent().children()[1]).children()[1]).attr("aria-valuetext",widget.options.radio);
-        $($($(" input[type=number]").parent().children()[1]).children()[1]).attr("aria-valuenow",widget.options.radio);
-        $($($(" input[type=number]").parent().children()[1]).children()[1]).attr("title",widget.options.radio);
-        if(!($(" input[type=checkbox]").parent().hasClass("ui-flipswitch-active")) && widget.options.gpsOnOff){
+$($(" input[type=number]").parent().children()[1]).children()[0].style.width = porcentajeBarra + "%";
+$($(" input[type=number]").parent().children()[1]).children()[1].style.left = porcentajeBarra + "%";
+        $($($(" input[type=number]").parent().children()[1]).children()[1]).attr("aria-valuetext", widget.options.radio);
+        $($($(" input[type=number]").parent().children()[1]).children()[1]).attr("aria-valuenow", widget.options.radio);
+        $($($(" input[type=number]").parent().children()[1]).children()[1]).attr("title", widget.options.radio);
+        if (!($(" input[type=checkbox]").parent().hasClass("ui-flipswitch-active")) && widget.options.gpsOnOff) {
             $(" input[type=checkbox]").parent().addClass("ui-flipswitch-active");
-        }
-        else if($(" input[type=checkbox]").parent().hasClass("ui-flipswitch-active") && !widget.options.gpsOnOff){
+        } else if ($(" input[type=checkbox]").parent().hasClass("ui-flipswitch-active") && !widget.options.gpsOnOff) {
             $(" input[type=checkbox]").parent().removeClass("ui-flipswitch-active");
         }
-	},
-	guardarValoresSeleccionados : function () {
+    },
+    guardarValoresSeleccionados: function () {
         var widget = this;
         widget.options.tipoVehiculoId = $('select option:selected').val()
         widget.options.radio = $(" input[type=number]").val();
         widget.options.gpsOnOff = $(" input[type=checkbox]").parent().hasClass("ui-flipswitch-active");
         var configuraciones = {
-            tipoVehiculo : widget.options.tipoVehiculoId,
-            radio : widget.options.radio,
-            gps : widget.options.gpsOnOff
+            tipoVehiculo: widget.options.tipoVehiculoId,
+            radio: widget.options.radio,
+            gps: widget.options.gpsOnOff
         };
         localStorage.setItem("Configuraciones", JSON.stringify(configuraciones));
-        
+
         widget.destroy();
-	},
-	_obtenerTiposVehiculosDeServidor : function (){
-		var uri = obtenerURLServer() + "api/playas/GetTiposVehiculo";
-		var tiposVehiculos = null;
-		$.ajax({
-			type: "GET",
-			async: false,
-			url: uri,
-			success: function (response) {
-				tiposVehiculos = jQuery.parseJSON(response);
-			}
-		});
-		return tiposVehiculos;
-	},
-	_agregarTiposDeVehiculos : function() {
-		var widget = this;
-		var tiposVehiculos = widget._obtenerTiposVehiculosDeServidor();
-		for(var i = 0; i < tiposVehiculos.length; i++){
-			var opcion = document.createElement("option");
-			opcion.value = tiposVehiculos[i].Id;
-			opcion.innerHTML = tiposVehiculos[i].Nombre;
-			 $('#tipoVehiculoSelect').append(opcion);
-		}
-	}
+    },
+    _obtenerTiposVehiculosDeServidor: function () {
+        var uri = obtenerURLServer() + "api/playas/GetTiposVehiculo";
+        var tiposVehiculos = null;
+        $.ajax({
+            type: "GET",
+            async: false,
+            url: uri,
+            success: function (response) {
+                tiposVehiculos = jQuery.parseJSON(response);
+            }
+        });
+        return tiposVehiculos;
+    },
+    _agregarTiposDeVehiculos: function () {
+        var widget = this;
+        var tiposVehiculos = widget._obtenerTiposVehiculosDeServidor();
+        for (var i = 0; i < tiposVehiculos.length; i++) {
+            var opcion = document.createElement("option");
+            opcion.value = tiposVehiculos[i].Id;
+            opcion.innerHTML = tiposVehiculos[i].Nombre;
+            $('#tipoVehiculoSelect').append(opcion);
+        }
+    }
 });
