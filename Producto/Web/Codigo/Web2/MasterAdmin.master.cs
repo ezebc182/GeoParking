48674a7f93cdc705aeed84bd750917d40fbce9e2
Iraki2014 <marcos.barrera90@gmail.com>
2015-02-05 12:04:26 -0300

    Documentos varios
    Manual usuario web, backlog completo, actualizacion de sprints

--- Producto/Web/Codigo/Web2/MasterAdmin.master.cs
+++ Producto/Web/Codigo/Web2/MasterAdmin.master.cs
@@ -46,10 +46,13 @@ namespace Web2
             {
                 SessionUsuario = resultado;
                 lblLogin.Text = SessionUsuario.NombreUsuario;
-                //Response.Redirect(Request.Url.AbsolutePath);
+                rolId = SessionUsuario.RolId;
+                if (rolId == 3 || rolId == 2)
+                {
+                    li_Administracion.Visible = true;
+                }
                 li_Ingresar.Visible = false;
                 li_Login.Visible = true;
-                li_Administracion.Visible = true;
             }
             else
             {
